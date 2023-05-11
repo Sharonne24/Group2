d2f6a5387b49f649060759198cb6323de1422c1e
Sharonne24 <sawuor395@gmail.com>
2023-05-11 20:01:41 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import Form from "./components/Form";
+import { Card } from "./components/Card";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
